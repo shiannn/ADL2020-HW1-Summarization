ef39f02822a7695a3fe5ae51f88d9eb72ea7947e
shiannn <b05502087@ntu.edu.tw>
2020-04-01 19:44:44 +0800

    success preprocessing embedding word2idx train.pkl valid.pkl test.pkl

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-pip install -r requirements.txt
-python -m nltk.downloader all
-python -m spacy download en_core_web_sm
+/#!/usr/bin/env bash
+pip3 install -r requirements.txt
+python3 -m nltk.downloader all
+python3 -m spacy download en_core_web_sm
