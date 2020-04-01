a7cc9b696f332ffb6395b02d25778d06c8e6e5fc
shiannn <b05502087@ntu.edu.tw>
2020-04-01 23:51:52 +0800

    finish porting

--- src/predict.py
+++ src/predict.py
@@ -5,12 +5,15 @@ import torch.optim as optim
 import numpy as np
 import json
 import sys
+import pickle
 #from RNN import LSTMTagger
 #from RNN import HIDDEN_DIM, EMBEDDING_DIM
-from BidirectionLstm import LSTMTagger
-from BidirectionLstm import HIDDEN_DIM, EMBEDDING_DIM
-from usedataset import SeqTaggingDataset
+from seq2tag.BidirectionLSTM import LSTMTagger
+from seq2tag.BidirectionLSTM import HIDDEN_DIM, EMBEDDING_DIM
 from torch.utils.data import Dataset, DataLoader
+import torch.utils.data as Data
+from torch.nn import Sigmoid
+from dataset import SeqTaggingDataset
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -22,80 +25,91 @@ def prepare_sequence(seq, to_ix):
     idxs = [to_ix[w] if w in to_ix else 0 for w in seq]
     return torch.tensor(idxs, dtype=torch.long)
 
-def postprocessing(tag_scores, test):
-    output = {}
+def postprocessing(tag_scores,sent_range):
+    tag_scores = tag_scores.squeeze()
+    #label = label.squeeze()
     #print(tag_scores)
-    #print(test)
-    wordList = test[0]
-    splitInterval = test[1]
-    ratio = []
-    predict_sent_idx = []
-    loss_function = nn.BCEWithLogitsLoss()
-
-    for i in range(len(splitInterval)-1):
+    #print(label)
+    #loss_function = nn.BCEWithLogitsLoss()
+    #loss = loss_function(tag_scores, torch.tensor())
+    oneS = torch.ones(tag_scores.shape).to(device)
+    zeroS = torch.zeros(tag_scores.shape).to(device)
+    #print(oneS)
+    #print(zeroS)
+    f = Sigmoid()
+    predictLabel = torch.where(f(tag_scores)>0.5,oneS,zeroS)
+    #print(predictLabel)
+    #print(sent_range)
+    
+    maxRatio = -1
+    predict_sent_idx = -1
+    for i in range(len(sent_range[0])):
         #the i'th sentence
-        wordInSent = splitInterval[i+1] - splitInterval[i]
+        st = sent_range[0][i][0]
+        ed = sent_range[0][i][1]
+        wordInSent = ed - st
         #print('wordInSent',wordInSent)
-        wordselected = 0
-        for word_score in tag_scores[splitInterval[i]:splitInterval[i+1]]:
-            #print('loss on [0,1]',loss_function(word_score, TARGETONE))
-            #print('loss on [1,0]',loss_function(word_score, TARGETZERO))
-            if loss_function(word_score, TARGETONE).item() < loss_function(word_score, TARGETZERO).item():
-                wordselected += 1
-            #elif loss_function(word_score, [0,1]) > loss_function(word_score, [1,0])::
-            """
-            if word_score[0] < word_score[1]:
-                #predict_sent_idx.append(i)
-                wordselected += 1
-            """
-        ratio.append(wordselected/wordInSent)
-    predict_sent_idx.append(ratio.index(max(ratio)))
-    return predict_sent_idx
+        wordselected = (predictLabel[st:ed]==1).sum()
+        #print('wordselected',wordselected)
+        ratio = wordselected / wordInSent
+        if ratio > maxRatio:
+            predict_sent_idx = i
+            maxRatio = ratio
+    return [predict_sent_idx]
 
 if __name__ == '__main__':
-    if(len(sys.argv)!=4):
-        print('usage: python3 predict.py model.pt TestingData.npy predict.jsonl')
+    if(len(sys.argv)!=5):
+        print('usage: python3 predict.py model.pt embedding.pkl TestingData.pkl predict.jsonl')
         exit(0)
     
+    modelName = sys.argv[1]
+    embeddingName = sys.argv[2]
+    testDataName = sys.argv[3]
+    predictName = sys.argv[4]
     # sys.argv[1] embedding (TrainingData.npy)
     # sys.argv[2] TestX (TestingData.npy)
     # sys.argv[3] TestY predict result (predict.jsonl)
 
-    testing_data = np.load(sys.argv[2], allow_pickle=True)
-    seqTaggingDataset = SeqTaggingDataset(testing_data)
+    with open(testDataName,"rb") as FileTesting:
+        #print(sys.argv[1])
+        testingData = pickle.load(FileTesting)
+
+    testingData = SeqTaggingDataset(testingData)
+
+    BATCH_SIZE = 1
+    loader = Data.DataLoader(
+        dataset=testingData,      # torch TensorDataset format
+        batch_size=BATCH_SIZE,      # mini batch size
+        shuffle=True,               # 要不要打乱数据 (打乱比较好)
+        num_workers=2,              # 多线程来读数据
+        collate_fn=testingData.collate_fn
+    )
+
+    with open(embeddingName, 'rb') as f:
+        embedding = pickle.load(f)
     #model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word2idx), 2, 'none') # yes/no 2
-    model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(seqTaggingDataset.word2idx), 2, 'none') # yes/no 2
-    model.load_state_dict(torch.load(sys.argv[1]))
+    model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(embedding.vocab), 1, 'none') # yes/no 2
+    model.load_state_dict(torch.load(modelName))
     model = model.to(device)
     model.eval()
 
-# 15779 出問題
-
     with torch.no_grad():
-        with open(sys.argv[3],'w') as f_predict:
-            for cnt,test in enumerate(testing_data):
-                Id = test[2]
-                toWrite = {}
-                inputs = prepare_sequence(test[0], seqTaggingDataset.word2idx)
-                inputs = inputs.to(device)
-                print(inputs.shape)
-                tag_scores = model(inputs)
-                #print(tag_scores)
-                predict_sent_idx = postprocessing(tag_scores, test)
-                print(Id, predict_sent_idx)
-                toWrite["id"] = Id
-                toWrite["predict_sentence_index"] = predict_sent_idx
-                json.dump(toWrite, f_predict)
-                f_predict.write("\n")
-                """
+        with open(predictName,'w') as f_predict:
+            for cnt,batch in enumerate(loader):
                 try:
-                    inputs = prepare_sequence(test[0], word2idx)
-                    inputs = inputs.to(device)
-                    #print(test[0])
-                    tag_scores = model(inputs)
+                    X = batch['text']
+                    Y = batch['label']
+                    sentRange = batch['sent_range']
+                    Id = batch['id'][0]
+                    #print(X,Y,Id)
+                    X = X.to(device, dtype=torch.long)
+                    #print(X.shape)
+                    tag_scores = model(X)
                     #print(tag_scores)
-                    predict_sent_idx = postprocessing(tag_scores, test)
-                    print(Id, predict_sent_idx)
+                    predict_sent_idx = postprocessing(tag_scores, sentRange)
+                    print('predict_sent_idx', predict_sent_idx)
+                    
+                    toWrite = {}
                     toWrite["id"] = Id
                     toWrite["predict_sentence_index"] = predict_sent_idx
                     json.dump(toWrite, f_predict)
@@ -108,4 +122,3 @@ if __name__ == '__main__':
                     toWrite["predict_sentence_index"] = []
                     json.dump(toWrite, f_predict)
                     f_predict.write("\n")
-                """
