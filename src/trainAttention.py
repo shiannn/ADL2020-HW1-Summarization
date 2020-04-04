3c2a528776b0dd585e8cc60c56201808f6944da5
shiannn <b05502087@ntu.edu.tw>
2020-04-05 01:32:37 +0800

    succeed run the attention train attention attention encoder attention decoder

--- src/trainAttention.py
+++ src/trainAttention.py
@@ -6,8 +6,8 @@ from torch import optim
 import torch.nn as nn
 import pickle
 import json
-from seq2seq.DecoderRNN import DecoderRNN
-from seq2seq.EncoderRNN import EncoderRNN, hidden_size
+from seq2seq.AttentionDecoder import AttnDecoderRNN
+from seq2seq.AttentionEncoder import AttnEncoderRNN, hidden_size
 import random
 import matplotlib.pyplot as plt
 
@@ -33,26 +33,36 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
     input_length = input_tensor.size(0)
     target_length = target_tensor.size(0)
 
-    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+    ### for the longest sentence
+    encoder_outputs = torch.zeros(max_length, BATCH_SIZE, encoder.hidden_size, device=device)
 
     TotalLoss = 0
 
     # input_tensor (seq_len, batch_size)
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            input_tensor[ei], encoder_hidden)
+        ### encoder_output (seq_len, batch_size, hidden_size)
+        encoder_outputs[ei] = encoder_output[0]
+    """
     encoder_output, encoder_hidden = encoder(
         input_tensor, encoder_hidden)
+    """
+    ### encoder_outputs (maxLen, batch, hidden_size)
 
     #decoder_input = torch.tensor([[SOS_token]]* BATCH_SIZE, device=device)
     decoder_input = torch.tensor([SOS_token]* BATCH_SIZE, device=device)
 
     decoder_hidden = encoder_hidden
+    # (num_layers*num_directions, batch_size, hidden_size)
 
     for di in range(target_length):
         #decoder_output, decoder_hidden, decoder_attention = decoder(
         #    decoder_input, decoder_hidden, encoder_outputs)
         #decoder_output, decoder_hidden, decoder_attention = decoder(
         #    decoder_input, decoder_hidden)
-        decoder_output, decoder_hidden = decoder(
-            decoder_input, decoder_hidden)
+        decoder_output, decoder_hidden, decoder_attention = decoder(
+            decoder_input, decoder_hidden, encoder_outputs)
         #print('decoder_output')
         #print(decoder_output)
         #print('decoder_output.shape', decoder_output.shape)
@@ -100,11 +110,10 @@ if __name__ == '__main__':
     with open(trainingName,"rb") as FileTraining:
         #print(sys.argv[1])
         trainingData = pickle.load(FileTraining)
-    """
+    
     with open(validName,"rb") as FileValidating:
         #print(sys.argv[1])
         validData = pickle.load(FileValidating)
-    """
 
     """
     with open("data/valid.jsonl","r") as f:
@@ -113,13 +122,22 @@ if __name__ == '__main__':
     """
     
     trainingData = Seq2SeqDataset(trainingData)
-    #validData = Seq2SeqDataset(validData)
+    validData = Seq2SeqDataset(validData)
 
     with open(embeddingName, 'rb') as f:
         embedding = pickle.load(f)
     
-    encoder = EncoderRNN(len(embedding.vocab), hidden_size, embedding.vectors, BATCH_SIZE).to(device)
-    decoder = DecoderRNN(hidden_size, len(embedding.vocab), embedding.vectors, BATCH_SIZE).to(device)
+    with open('../datasets/seq2seq/config.json', 'r') as f:
+        config = json.load(f)
+    
+    maxTextLen = config.get('max_text_len')
+    maxSummaryLen = config.get('max_summary_len')
+    print(maxTextLen, maxSummaryLen)
+    maxLength = max(maxTextLen, maxSummaryLen)
+
+    encoder = AttnEncoderRNN(len(embedding.vocab), hidden_size, embedding.vectors, BATCH_SIZE).to(device)
+    #decoder = DecoderRNN(hidden_size, len(embedding.vocab), embedding.vectors, BATCH_SIZE).to(device)
+    decoder = AttnDecoderRNN(hidden_size, len(embedding.vocab), embedding.vectors, BATCH_SIZE, maxLength, dropout_p=0.1).to(device)
 
     loader = Data.DataLoader(
         dataset=trainingData,      # torch TensorDataset format
@@ -129,22 +147,13 @@ if __name__ == '__main__':
         drop_last=True,
         collate_fn=trainingData.collate_fn,
     )
-    
-    #maxSummaryLen = max([len(sample['summary']) for sample in trainingData])
-    #maxTextLen = max([len(sample['text']) for sample in trainingData])
-    with open('../datasets/seq2seq/config.json') as f:
-        config = json.load(f)
-    
-    maxTextLen = config.get('max_text_len')
-    maxSummaryLen = config.get('max_summary_len')
-    print(maxTextLen, maxSummaryLen)
+
     
     learning_rate=0.001
     encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
     decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
     criterion = nn.NLLLoss(reduction='none')
 
-    max_length = max(maxTextLen, maxSummaryLen)
     print('hello')
     epochError = []
     tempError = []
@@ -155,13 +164,13 @@ if __name__ == '__main__':
             #target_tensor = batch['summary'].reshape(-1,1).to(device)
             input_tensor = batch['text'].to(device)
             target_tensor = batch['summary'].to(device)
-            loss = train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length, embedding)
+            loss = train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, maxLength, embedding)
             print('training loss {} epoch {}/{} {}/{}'.format(loss, epoch, EPOCH, i*BATCH_SIZE, len(loader.dataset)))
             tempError.append(loss)
         epochError.append(sum(tempError)/len(tempError))
         tempError = []
 
         plt.plot(epochError)
-        plt.savefig('figplot_seq2seq/'+'1'+'-'+'1'+'.png')    
-        torch.save(encoder.state_dict(), 'checkpoint_seq2seq/'+'encoder'+str(epoch)+'.pt')
-        torch.save(decoder.state_dict(), 'checkpoint_seq2seq/'+'decoder'+str(epoch)+'.pt')
+        plt.savefig('figplot_attention/'+'1'+'-'+'1'+'.png')    
+        torch.save(encoder.state_dict(), 'checkpoint_attention/'+'encoder'+str(epoch)+'.pt')
+        torch.save(decoder.state_dict(), 'checkpoint_attention/'+'decoder'+str(epoch)+'.pt')
